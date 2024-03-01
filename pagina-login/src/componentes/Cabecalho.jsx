b8c6aa5ea3ef191b8541c3a85c9e6f2157be0407
Danielle Nunes <141048448+Danielle-sn@users.noreply.github.com>
2024-03-01 15:33:23 -0300

    pagina-login-Danielle

---
+++ pagina-login/src/componentes/Cabecalho.jsx
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom'
+import schimg from '../img/sch.svg';
+// import icone_menu from '../img/icone_menu.png';
+
+import styles from './Cabecalho.module.css';
+
+//Cabeçalho que Benny fez com algumas alterações
+
+function Cabecalho(){
+    const [mostrarInfo, setMostrarInfo] = useState(false);
+  
+    const Info = () => {
+      setMostrarInfo(!mostrarInfo);
+    }
+    return(
+      <header>
+        <div className={styles.cabecalho}>
+          <img className={styles.img_logo_sch} src={schimg} alt='logo do sch'></img>
+          {/* <button className={styles.botao_menu} onClick={Info}><img className={styles.img_botao_menu} src={icone_menu} alt = 'botão para exibir as opçoes de navegação'></img></button> */}
+          {/* acho que ficaria mais organizado pra versão mobile usar  o botão  */}
+          {/* {mostrarInfo && ( */}
+            <div className={styles.opcoes_do_menu}>
+              <ul>
+                <li className={styles.item}>
+                  <Link to= "/">Início</Link>
+                </li>
+                <li className={styles.item}>
+                  <Link to= "/Servicos">Serviços</Link>
+                </li>
+                <li className={styles.item}>
+                  <Link to= "/Entrar">Entrar</Link>
+                </li>
+              </ul>
+              
+            </div>
+          {/* )} */}
+        </div>
+      </header>
+    );
+  }
+
+  export default Cabecalho 
---
+++ pagina-login/src/componentes/form/LoginForm.jsx
@@ -0,0 +1,40 @@
+import {Link} from 'react-router-dom'
+import styles from './LoginForm.module.css';
+import Input from '../Input' 
+
+function LoginForm(){
+  return(
+    <div className={styles.form}>
+    <h1>Resultados de Exames</h1>
+    <p>Login</p>
+    <form >
+      <Input
+      type="text"
+      text="CPF:"
+      name="name"
+      placeholder="Insira seu CPF" 
+      />
+      <Input
+      type="password"
+      text="Senha:"
+      name="password"
+      placeholder="Insira sua senha" 
+      />
+      <li className={styles.red_senha}>
+      <Link  to= "/RedefinirSenha">Redefinir senha</Link>
+      </li>
+      <li className={styles.botao}>
+        <Link to="/Exames">Entrar</Link>
+      </li>
+      <li className={styles.cadastro}>
+      <Link  to= "/Cadastro">ME CADASTRAR</Link>
+      </li>
+    </form>
+    </div>
+  )
+}
+
+
+
+export default LoginForm
+

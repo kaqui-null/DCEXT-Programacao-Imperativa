0e7b0150958419a4c5ed16f5bd97e40afd55c920
Biazita14 <beatriz.bbbferraz@gmail.com>
2024-03-03 17:16:55 -0300

    inicializou servidor

---
+++ pagina-login/src/client/src/componentes/pages/Medico.jsx
@@ -0,0 +1,10 @@
+import LoginForm from '../form/LoginForm'
+
+function Medico(){
+  return(
+    // coloquei dentra de uma div que não tava centralizada ai o coisa ficou pro lado 
+    <LoginForm/>
+  )
+}
+
+export default Medico

7ac699cc36f08791acb83110d6b6885fffc3a3ee
CarlosWilsonNT <carloswilson223@gmail.com>
2024-03-13 20:52:36 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ExamePage from './ExamePage';
+
+function App() {
+  return (
+    <div className="App">
+      <ExamePage />
+    </div>
+  );
+}
+
+export default App;

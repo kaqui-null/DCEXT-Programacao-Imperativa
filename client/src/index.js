f5f528567ce714ec7d8acdad365d8ae061a1e1ee
kaqui-null <emanuelgomes24.08@gmail.com>
2024-03-20 00:03:24 -0300

    fix bad commit

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import PaginaMedico from './PaginaMedico';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < PaginaMedico />
+    < HistoricoGeral />
   </React.StrictMode>
 );
 
